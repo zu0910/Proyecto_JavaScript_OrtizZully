d69e9176cb35703ed56f95622fafe79435865088
zu0910 <zullyortiz.09@gmail.com>
2024-08-20 12:54:18 -0500

    feat :sparkles: Start code of javaScript

---
+++ script.js
@@ -0,0 +1,564 @@
+var campuland = [
+    {
+        "Campers": [
+            {
+                "id": 5708,
+                "user": {
+                    "login": "Luis_01",
+                    "contrase\u00f1a": "luorheber03"
+                },
+                "numeroIdentificacion": 10939352265,
+                "nombres": "Luis Jose",
+                "apellidos": "Bemon Henao",
+                "direccion": "Diocesano",
+                "acudiente": "Franklin Henao",
+                "telefonoCelular": 322890392,
+                "telefonoFijo": 3452331,
+                "tiposDeEstados": [
+                    "En proceso",
+                    "Inscrito",
+                    "Aprobado",
+                    "Cursando",
+                    "Graduado",
+                    "Expulsado",
+                    "Retirado"
+                ],
+                "estado": "Cursando",
+                "fechaInicio": "19-03-2024",
+                "fechaCierre": "19-03-2025",
+                "grupo": "M1",
+                "ruta": "JavaScript",
+                "moduloActual": 1,
+                "trainer": "",
+                "riesgo": [
+                    "Bajo",
+                    "Alto",
+                    "Bajo",
+                    "Alto",
+                    "Bajo"
+                ],
+                "notaFiltro": [
+                    75.0,
+                    43.0,
+                    71.5,
+                    49.5,
+                    68.5
+                ]
+            },
+            {
+                "id": 123,
+                "user": {
+                    "login": "zu123",
+                    "contrase\u00f1a": "fer123"
+                },
+                "numeroIdentificacion": 10936265354,
+                "nombres": "Zully Fernanda",
+                "apellidos": "Ortiz Avenda\u00f1o",
+                "direccion": "pinos",
+                "acudiente": "Zully Fabiola Avenda\u00f1o",
+                "telefonoCelular": 153513513,
+                "telefonoFijo": 513135135,
+                "notaAprobacion": 50,
+                "tiposDeEstados": [
+                    "En proceso",
+                    "Inscrito",
+                    "Aprobado",
+                    "Cursando",
+                    "Graduado",
+                    "Expulsado",
+                    "Retirado"
+                ],
+                "estado": "Aprobado",
+                "fechaInicio": "15-02-2024",
+                "fechaCierre": "15-02-2025",
+                "grupo": "M1",
+                "ruta": "JavaScript",
+                "moduloActual": 1,
+                "trainer": "",
+                "riesgo": [
+                    "Alto",
+                    "Bajo",
+                    "Alto",
+                    "Alto",
+                    "Bajo"
+                ],
+                "notaFiltro": [
+                    41.5,
+                    77.5,
+                    48.0,
+                    46.0,
+                    74.0
+                ]
+            },
+            {
+                "id": 333,
+                "user": {
+                    "login": "jhoan37",
+                    "contrase\u00f1a": "jhoan372"
+                },
+                "numeroIdentificacion": 1502065,
+                "nombres": "jhoan stiven",
+                "apellidos": "Bermon Garay",
+                "direccion": "Camilo torres",
+                "acudiente": "Blanca Smith Bermon Garay",
+                "telefonoCelular": 56523506,
+                "telefonoFijo": 846846541,
+                "NotaI": 0,
+                "tiposDeEstados": [
+                    "En proceso",
+                    "Inscrito",
+                    "Aprobado",
+                    "Cursando",
+                    "Graduado",
+                    "Expulsado",
+                    "Retirado"
+                ],
+                "estado": "Aprobado",
+                "fechaInicio": "14-02-2024",
+                "fechaCierre": "14-12-2024",
+                "grupo": "T3",
+                "ruta": "Netcore",
+                "moduloActual": 1,
+                "trainer": "Laura Sofia",
+                "riesgo": [
+                    "Bajo",
+                    "",
+                    "",
+                    "",
+                    ""
+                ],
+                "notaFiltro": [
+                    64.5,
+                    0,
+                    0,
+                    0,
+                    0
+                ]
+            }
+        ],
+        "Trainers": [
+            {
+                "id": 75458546,
+                "user": {
+                    "login": "Pedro_10",
+                    "contrase\u00f1a": "Pedrito"
+                },
+                "numeroIdentificacion": 88563265,
+                "nombres": "Pedro jose",
+                "apellidos": "Perez Sanchez",
+                "direccion": "Bogota",
+                "telefonoCelular": 3266598821,
+                "telefonoFijo": 9862656518,
+                "ruta": "Nodejs",
+                "grupo": [
+                    "T1"
+                ]
+            },
+            {
+                "id": 1523,
+                "user": {
+                    "login": "Michael_03",
+                    "contrase\u00f1a": "Michael_010"
+                },
+                "numeroIdentificacion": 88563265,
+                "nombres": "Michael Andres",
+                "apellidos": "Garzon Ramirez",
+                "direccion": "Bogota",
+                "telefonoCelular": 342100456,
+                "telefonoFijo": 58416543521,
+                "ruta": "Java",
+                "grupo": [
+                    "T2"
+                ]
+            },
+            {
+                "id": 1222,
+                "user": {
+                    "login": "laura03",
+                    "contrase\u00f1a": "laura031"
+                },
+                "numeroIdentificacion": 88563265,
+                "nombres": "Laura Sofia",
+                "apellidos": "Salamanca Zuleta",
+                "direccion": "Tibu",
+                "telefonoCelular": 5641651,
+                "telefonoFijo": 8541651561,
+                "ruta": "Netcore",
+                "grupo": [
+                    "T3"
+                ]
+            },
+            {
+                "id": 98219389,
+                "user": {
+                    "login": "blanca03",
+                    "contrase\u00f1a": "blanquita"
+                },
+                "numeroIdentificacion": 1298391823,
+                "nombres": "Blanca Smith",
+                "apellidos": "Bermon Garay",
+                "direccion": "Camilo torres",
+                "telefonoCelular": 983102938,
+                "telefonoFijo": 8129389123,
+                "ruta": "",
+                "grupo": [
+                    ""
+                ]
+            },
+            {
+                "id": 839012930,
+                "user": {
+                    "login": "orbin897",
+                    "contrase\u00f1a": "0r4289"
+                },
+                "numeroIdentificacion": 82387239,
+                "nombres": "Orbin jose",
+                "apellidos": "Sanchez Ramirez",
+                "direccion": "Cucuta",
+                "telefonoCelular": 983108972938,
+                "telefonoFijo": 1239809132,
+                "ruta": "",
+                "grupo": [
+                    ""
+                ]
+            },
+            {
+                "id": 23918023,
+                "user": {
+                    "login": "elianita98",
+                    "contrase\u00f1a": "eliana2000"
+                },
+                "numeroIdentificacion": 2139802893,
+                "nombres": "Elian Jose",
+                "apellidos": "Bermon Garay",
+                "direccion": "Las minas",
+                "telefonoCelular": 839208192,
+                "telefonoFijo": 9872193721,
+                "ruta": "",
+                "grupo": [
+                    ""
+                ]
+            }
+        ],
+        "Coordinador": [
+            {
+                "id": 12347,
+                "user": {
+                    "login": "stiv1",
+                    "contrase\u00f1a": "stiv12"
+                },
+                "numeroIdentificacion": 9128371287,
+                "nombres": "Brayan Stiven",
+                "apellidos": "Carvajal Romero",
+                "direccion": "Bucaramanga",
+                "telefonoCelular": 32782938,
+                "telefonoFijo": 1230989
+            },
+            {
+                "numeroModulo": [
+                    "Fundamentos de programacion",
+                    "Programacion web",
+                    "Programacion Formal",
+                    "Bases de datos",
+                    "Backend"
+                ],
+                "materiasM3": [
+                    "Java",
+                    "JavaScript",
+                    "C#"
+                ],
+                "materiasM4": [
+                    "Mysql",
+                    "MongoDb",
+                    "Postgresql"
+                ],
+                "materiasM5": [
+                    "Netcore",
+                    "Spring Boot",
+                    "NodeJs",
+                    "Express"
+                ],
+                "rutas": [
+                    "Nodejs",
+                    "Java",
+                    "Netcore",
+                    "JavaScript"
+                ],
+                "tiposDeRutas": [
+                    {
+                        "nombreRuta": "Nodejs",
+                        "grupos": [
+                            "T1"
+                        ],
+                        "modulos": [
+                            [
+                                "Introduccion a la algoritmo",
+                                "Pseint",
+                                "Python"
+                            ],
+                            [
+                                "HTML",
+                                "CSS",
+                                "Bootstrap"
+                            ],
+                            [
+                                "Java",
+                                "JavaScript"
+                            ],
+                            [
+                                "Mysql",
+                                "Postgresql"
+                            ],
+                            [
+                                "NodeJs",
+                                "Express"
+                            ]
+                        ]
+                    },
+                    {
+                        "nombreRuta": "Java",
+                        "grupos": [
+                            "T2"
+                        ],
+                        "modulos": [
+                            [
+                                "Introduccion a la algoritmo",
+                                "Pseint",
+                                "Python"
+                            ],
+                            [
+                                "HTML",
+                                "CSS",
+                                "Bootstrap"
+                            ],
+                            [
+                                "Java",
+                                "JavaScript"
+                            ],
+                            [
+                                "Mysql",
+                                "Postgresql"
+                            ],
+                            [
+                                "NodeJs",
+                                "Express"
+                            ]
+                        ]
+                    },
+                    {
+                        "nombreRuta": "Netcore",
+                        "grupos": [
+                            "T3"
+                        ],
+                        "modulos": [
+                            [
+                                "Introduccion a la algoritmo",
+                                "Pseint",
+                                "Python"
+                            ],
+                            [
+                                "HTML",
+                                "CSS",
+                                "Bootstrap"
+                            ],
+                            [
+                                "Java",
+                                "JavaScript"
+                            ],
+                            [
+                                "Mysql",
+                                "Postgresql"
+                            ],
+                            [
+                                "NodeJs",
+                                "Express"
+                            ]
+                        ]
+                    },
+                    {
+                        "nombreRuta": "JavaScript",
+                        "grupos": [
+                            "M1"
+                        ],
+                        "modulos": [
+                            [
+                                "Introduccion a la algoritmo",
+                                "Pseint",
+                                "Python"
+                            ],
+                            [
+                                "HTML",
+                                "CSS",
+                                "Bootstrap"
+                            ],
+                            [
+                                "Java"
+                            ],
+                            [
+                                "Mysql",
+                                "MongoDb"
+                            ],
+                            [
+                                "Netcore"
+                            ]
+                        ]
+                    }
+                ],
+                "grupos": [
+                    "T1",
+                    "T2",
+                    "T3",
+                    "M1",
+                    "M2",
+                    "M3"
+                ]
+            }
+        ]
+    }
+]
+
+// Munu principal de los campers
+function menuCampers() {
+    console.log("");
+    console.log(
+        "/////////////////////////////////////////////////\n" +
+        "------------ BIENVENIDO CAMPER-------------------\n" +
+        "1). Reportes.\n" +
+        "2). Cambiar usuarios y contraseñas.\n" +
+        "3). Cambiar informacion.\n" +
+        "-------------------------------------------------\n" +
+        "/////////////////////////////////////////////////"
+    );
+};
+
+
+// Opciones del menu de la opcion 1 del camper
+function menuCamperOpc1() {
+    console.log("");
+    console.log(
+        "---------------------REPORTES---------------------\n" +
+        "1). Mostrar datos del Camper.\n" +
+        "2). Riesgo del camper.\n" +
+        "3). Ruta del camper.\n" +
+        "4). Trainer del camper\n" +
+        "5). Modulo en el que se encuetra el camper.\n" +
+        "-------------------------------------------------\n"
+    );
+};
+
+
+// Opciones del menu de la opcion 3 del camper
+function menuCampersOpc3() {
+    console.log("");
+    console.log(
+        "------------ BIENVENIDO CAMPER-------------------\n" +
+        "1). Cambiar dirección..\n" +
+        "2). Cambiar telefono movil.\n" +
+        "3). Cambiar telefono fijo.\n" +
+        "-------------------------------------------------\n"
+    );
+};
+
+
+// Menu principal del trainer 
+function menuTrainer() {
+    console.log("");
+    console.log(
+        "///////////////////////////////////////////////////\n"+
+        "----------------- BIENVENIDO TRAINER --------------\n" +
+        "1). Cambiar usuario y contraseña.\n" +
+        "2). Ruta de entrenamiento.\n" +
+        "3). Cambiar información.\n" +
+        "-------------------------------------------------\n"+
+        "///////////////////////////////////////////////////"
+    );
+};
+
+
+// Opciones del menu de la opcion 3 del trainer
+function menuTrainerOpc3() {
+    console.log("");
+    console.log(
+        "----------------- Cambiar información ---------------\n" +
+        "1). Cambiar dirección..\n" +
+        "2). Cambiar telefono movil.\n" +
+        "3). Cambiar telefono fijo.\n" +
+        "-------------------------------------------------\n"
+    );
+};
+
+
+// Menu principal del coordinador 
+function menuCoordinador() {
+    console.log("");
+    console.log(
+        "////////////////////////////////////////////////////////////////\n"+
+        "-------------------- Bienvenido coordinador --------------------\n" +
+        "1). Cambiar usuarios y contraseñas de todo el sistema educativo.\n" +
+        "2). Cambiar informacion de todo el sistema educativo.\n" +
+        "3). Agregar nota de examen de aprobación.\n" +
+        "4). Agregar nota de filtro. \n" +
+        "5). Consultar cuales campers se encuentra en riesgo alto.\n" +
+        "6). Reporte.\n"+
+        "7). Crear nuevas rutas.\n" +
+        "8). Crear nuevos campers.\n" +
+        "9). Salir.\n" +
+        "----------------------------------------------------------------\n"+
+        "////////////////////////////////////////////////////////////////"
+    );
+};
+
+
+// Menu de las 
+function menuCoordinadorOpc8() {
+    console.log("");
+    console.log(
+        "---------------------------------    Reporte   ----------------------------------------------------\n" +
+        "1). Listar los campers que se encuentre en estado inscrito\n" +
+        "2). Listar los campers que aprobaron el examen final.\n" +
+        "3). Listar los trainers que se encuentre trabajando en Campusland. \n" +
+        "4). Litar los campers que se encuentre en bajo rendimiento. \n" +
+        "5). Listar los campers y trainers que se encuentren asociados a una ruta de entretenimiento.\n" +
+        "6). Mostrar cuantos campers perdieron y aprobaron cada modulo. \n" +
+        "----------------------------------------------------------------\n"
+    );
+};
+
+let user=prompt("Ingrese su usuario: ");
+var x=0
+
+while (x==0){
+    for (const i of campuland[0]["Campers"]){
+        if (user==i["user"]["login"]){
+            x+=1
+            var passwordCamper=prompt("Ingresa su contraseña:\n")
+
+            while (passwordCamper != i["user"]["contraseña"]){
+                passwordCamper=prompt("Contraseña incorrecta ingresa otra vez\n")
+                break
+            }
+            menuCampers();
+            var opcMenuCamper= prompt("Ingresa una opcion de las que aparecen en pantalla")
+            var campers=true;
+            while (campers=true){
+                if (opcMenuCamper==1){
+                    menuCamperOpc1()
+                    var newOpc1=prompt("Ingrese una opcion del menu anterior")
+                    if (newOpc1==1){
+                        console.log("--- Hola",campuland[0]["Campers"][i]["nombres"],"estos son tus datos: -----------\n")
+                        console.log("    Numero de identificaion:",campuland[0]["Campers"][i]["numeroIdentificacion"])
+                        console.log("    Nombres:",campuland[0]["Campers"][i]["nombres"])
+                        console.log("    Apellidos:",campuland[0]["Campers"][i]["apellidos"])
+                        console.log("    Direccion:",campuland[0]["Campers"][i]["direccion"])
+                        console.log("    Acudiente:",campuland[0]["Campers"][i]["acudiente"])
+                        console.log("    Telefono celular:",campuland[0]["Campers"][i]["telefonoCelular"])
+                        console.log("    Telefono fijo:",campuland[0]["Campers"][i]["telefonoFijo"])
+                        console.logt("------------------------------------------------------")
+                        break
+                    }
+                }
+
+            }
+        }
+    }
+}
